858449590e73445e1586057e83c5b899579709ec
Calvin Baart <calvin.baart@gmail.com>
2024-09-09 16:40:31 +0200

    added libwebp

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install ninja
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   apt-get update
-  apt-get install -y ninja-build xorg-dev cmake g++ libharfbuzz-dev
+  apt-get install -y ninja-build xorg-dev cmake g++ libharfbuzz-dev libwebp-dev
 else
   choco install ninja
 fi
