46be6d924376bb623c3c1ac2332f71359ff11bee
Calvin Baart <calvinbaart@users.noreply.github.com>
2025-07-30 21:16:21 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,19 @@ if [ -f "$PWD/build/bin/aseprite" ]; then
   exit 0
 fi
 
+if [ "$(uname)" == "Darwin" ]; then
+  brew install ninja
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  apt-get update
+  apt-get install -y ninja-build xorg-dev cmake g++ libcurl4-gnutls-dev libharfbuzz-dev libwebp-dev gn libgif-dev libtiff5-dev libjpeg-dev libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev libtinyxml-dev libtinyxml2-dev libcmark-dev
+else
+  choco install ninja
+fi
+  apt-get install -y ninja-build xorg-dev cmake g++ libcurl4-gnutls-dev libharfbuzz-dev libwebp-dev gn libgif-dev libtiff5-dev libjpeg-dev libx11-dev libxcursor-dev libxi-dev libgl1-mesa-dev libfontconfig1-dev libtinyxml-dev libtinyxml2-dev libcmark-dev
+else
+  choco install ninja
+fi
+
 cd clone
 git checkout temp
 
